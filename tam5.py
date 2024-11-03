772111914b5e9e5d91504b0b8af7f298a700b4df
Amir Ali Ahmadi <amiraliandadrian92@gmail.com>
2024-11-03 22:59:01 +0330

    Update tam5.py

--- tam5.py
+++ tam5.py
@@ -1,8 +1,8 @@
 letter=input("please enter sentence: ")
 va="aoieuAOIEU"
 list1=[]
-for item in va:
-    if item in letter and item not in list1:
+for item in letter:
+    if item in va and item not in list1:
         list1.append(item)
 print(list1)        
         
